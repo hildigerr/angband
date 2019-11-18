9cd9f2137360253626e63207926af9d175fe28f0
Hildigerr Vergaray <Roberto.Vergaray@YmirSystems.com>
2019-11-18 00:35:49 -0800

    z-virt implements rnfree, ralloc, string_(make,free)

--- src/z-virt.c
+++ src/z-virt.c
@@ -21,3 +21,95 @@ char *memset(char *s, int c, unsigned long n)
 #endif
 
 
+
+
+/*
+ * Free some memory (that was allocated by ralloc).
+ */
+int rnfree(void *p, unsigned long len)
+{
+  /* Easy to free zero bytes */
+  if (len == 0) return (0);
+
+  /* just use "free" */
+  free ((char*)(p));
+
+  /* Success */
+  return (0);
+}
+
+
+/*
+ * Allocate some memory
+ */
+void *ralloc(unsigned long len)
+{
+  void *mem;
+
+  /* Allow allocation of "zero bytes" */
+  if (len == 0) return ((void *)NULL);
+
+  /* Use malloc() to allocate some memory */
+  mem = ((void *)(malloc((size_t)(len))));
+
+  /* We were able to acquire memory */
+  if (mem) return (mem);
+
+  /* Abort the system */
+  core("OUT OF MEMORY!!!");
+
+  /* Make the compiler happy */
+  return ((void *)NULL);
+}
+
+
+
+
+/*
+ * Allocate a constant string, containing the same thing as 'str'
+ */
+const char *string_make(const char *str)
+{
+  register unsigned long len = 0;
+  register const char *t = str;
+  register char *s, *res;
+
+  /* Simple sillyness */
+  if (!str) return (str);
+
+  /* Get the number of chars in the string, including terminator */
+  while (str[len++]);
+
+  /* Allocate space for the string */
+  s = res = (char*)(ralloc(len));
+
+  /* Copy the string (with terminator) */
+  while ((*s++ = *t++));
+
+  /* Return the allocated, initialized, string */
+  return (res);
+}
+
+
+/*
+ * Un-allocate a string allocated above.
+ * Depends on no changes being made to the string.
+ */
+int string_free(const char* str)
+{
+  register unsigned long len = 0;
+
+  /* Succeed on non-strings */
+  if (!str) return (0);
+
+  /* Count the number of chars in 'str' plus the terminator */
+  while (str[len++]);
+
+  /* Kill the buffer of chars we must have allocated above */
+  rnfree((vptr)(str), len);
+
+  /* Success */
+  return (0);
+}
+
+
--- src/z-virt.h
+++ src/z-virt.h
@@ -8,12 +8,28 @@
 /*
  * Memory management routines.
  *
+ * The string_make() and string_free() routines handle dynamic strings.
+ * A dynamic string is a string allocated at run-time, but not really
+ * intended to be modified once allocated.
+ *
  * Note that it is assumed that "memset()" will function correctly,
  * in particular, that it returns its first argument.
  */
 
 
 /**** Available Routines ****/
+
+/* De-allocate a given amount of memory */
+int rnfree(void *p, unsigned long len);
+
+/* Allocate (and return) 'len', or dump core */
+void *ralloc(unsigned long len);
+
+/* Create a "dynamic string" */
+const char *string_make(const char *str);
+
+/* Free a string allocated with "string_make()" */
+int string_free(const char* str);
 
 #ifndef HAS_MEMSET
 extern char *memset(char*, int, unsigned long);
