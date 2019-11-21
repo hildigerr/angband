86266bc0966e7a751491cac752f199fbe3e829ba
Hildigerr Vergaray <Roberto.Vergaray@YmirSystems.com>
2019-11-20 16:34:55 -0800

    cave.c has prt_map from misc1

--- src/misc1.c
+++ src/misc1.c
@@ -411,25 +411,6 @@ int y, x;
 
 
 
-/* Prints the map of the dungeon			-RAK-	 */
-void 
-prt_map()
-{
-    register int           i, j, k;
-    register unsigned char tmp_char;
-
-    k = 0;
-    for (i = panel_row_min; i <= panel_row_max; i++) {	/* Top to bottom */
-	k++;
-	erase_line(k, 13);
-
-	for (j = panel_col_min; j <= panel_col_max; j++) {	/* Left to right */
-	    tmp_char = loc_symbol(i, j);
-	    if (tmp_char != ' ')
-		print(tmp_char, i, j);
-	}
-    }
-}
 
 
 /* Compact monsters					-RAK-	 */
