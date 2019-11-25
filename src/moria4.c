47a93694197d2f7a14bf0441aa1f0010ee9c3a10
Hildigerr Vergaray <Roberto.Vergaray@YmirSystems.com>
2019-11-24 17:13:59 -0800

    init moria4 with mmove from misc2

---
+++ src/moria4.c
@@ -0,0 +1,93 @@
+/* File: moria4.c */
+
+/* Purpose: more low level code */
+
+/*
+ * Copyright (c) 1989 James E. Wilson, Robert A. Koeneke 
+ *
+ * This software may be copied and distributed for educational, research, and
+ * not for profit purposes provided that this copyright and statement are
+ * included in all such copies. 
+ */
+
+#include "angband.h"
+
+
+
+
+/*
+ * Given direction "dir", returns new row, column location -RAK-
+ * targeting code stolen from Morgul -CFT
+ * 'dir=0' moves toward target				    CDW  
+ */
+int mmove(int dir, int *y, int *x)
+{
+    register int new_row = 0, new_col = 0;
+    int          boolflag;
+
+    switch (dir) {
+
+#ifdef TARGET
+
+      case 0:
+	new_row = *y;
+	new_col = *x;
+	mmove2(&new_row, &new_col,
+	       char_row, char_col,
+	       target_row, target_col);
+
+	break;
+
+#endif /* TARGET */
+
+      case 1:
+	new_row = *y + 1;
+	new_col = *x - 1;
+	break;
+      case 2:
+	new_row = *y + 1;
+	new_col = *x;
+	break;
+      case 3:
+	new_row = *y + 1;
+	new_col = *x + 1;
+	break;
+      case 4:
+	new_row = *y;
+	new_col = *x - 1;
+	break;
+      case 5:
+	new_row = *y;
+	new_col = *x;
+	break;
+      case 6:
+	new_row = *y;
+	new_col = *x + 1;
+	break;
+      case 7:
+	new_row = *y - 1;
+	new_col = *x - 1;
+	break;
+      case 8:
+	new_row = *y - 1;
+	new_col = *x;
+	break;
+      case 9:
+	new_row = *y - 1;
+	new_col = *x + 1;
+	break;
+    }
+    boolflag = FALSE;
+    if ((new_row >= 0) && (new_row < cur_height)
+	&& (new_col >= 0) && (new_col < cur_width)) {
+	*y = new_row;
+	*x = new_col;
+	boolflag = TRUE;
+    }
+    return (boolflag);
+}
+
+
+
+
+
