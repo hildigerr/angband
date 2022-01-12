90cdbff8d4c59f8c26f7b096b0c672708bd599d1
Hildigerr Vergaray <Hildigerr.Vergaray@YmirSystems.com>
2022-01-12 11:04:01 -0800

    init effects with lose_{str,int,wis,dex,con,chr}

---
+++ src/effects.c
@@ -0,0 +1,111 @@
+/* File: effects.c */
+
+/* Purpose: effects of various "objects" */
+
+/*
+ * Copyright (c) 1989 James E. Wilson, Robert A. Koeneke 
+ *
+ * This software may be copied and distributed for educational, research, and
+ * not for profit purposes provided that this copyright and statement are
+ * included in all such copies. 
+ */
+
+#include "angband.h"
+
+
+
+/*
+ * XXX XXX XXX XXX Someone needs to verify all of these effects.
+ */
+
+
+
+/*
+ * Lose a strength point.				-RAK-	 
+ */
+static void lose_str()
+{
+    if (!py.flags.sustain_str) {
+	(void)dec_stat(A_STR);
+	msg_print("You feel very weak.");
+    }
+    else {
+	msg_print("You feel weak for a moment;  it passes.");
+    }
+}
+
+
+/*
+ * Lose an intelligence point.				-RAK-	 
+ */
+static void lose_int()
+{
+    if (!py.flags.sustain_int) {
+	(void)dec_stat(A_INT);
+	msg_print("You become very dizzy.");
+    }
+    else {
+	msg_print("You become dizzy for a moment;  it passes.");
+    }
+}
+
+
+/*
+ * Lose a wisdom point.					-RAK-	 
+ */
+static void lose_wis()
+{
+    if (!py.flags.sustain_wis) {
+	(void)dec_stat(A_WIS);
+	msg_print("You feel very naive.");
+    }
+    else {
+	msg_print("You feel naive for a moment;  it passes.");
+    }
+}
+
+
+/*
+ * Lose a dexterity point.				-RAK-	 
+ */
+static void lose_dex()
+{
+    if (!py.flags.sustain_dex) {
+	(void)dec_stat(A_DEX);
+	msg_print("You feel very sore.");
+    }
+    else {
+	msg_print("You feel sore for a moment;  it passes.");
+    }
+}
+
+
+/*
+ * Lose a constitution point.				-RAK-	 
+ */
+static void lose_con()
+{
+    if (!py.flags.sustain_con) {
+	(void)dec_stat(A_CON);
+	msg_print("You feel very sick.");
+    }
+    else {
+	msg_print("You feel sick for a moment;  it passes.");
+    }
+}
+
+
+/*
+ * Lose a charisma point.				-RAK-	 
+ */
+static void lose_chr()
+{
+    if (!py.flags.sustain_chr) {
+	(void)dec_stat(A_CHR);
+	msg_print("Your skin starts to itch.");
+    }
+    else {
+	msg_print("Your skin starts to itch, but feels better now.");
+    }
+}
+
