5aff479823fd8a635354d11ae22c3f94d327db1c
Hildigerr Vergaray <Hildigerr.Vergaray@YmirSystems.com>
2024-08-15 19:38:51 -0700

    config: rearrange options

--- src/config.h
+++ src/config.h
@@ -20,16 +20,48 @@
  * wishes.
  */
 
-#undef CHECK_HOURS            /* define if checking the 'hours' file          */
-#define ALLOW_FIDDLING       /* Allow the players to copy save files         */
 #define ALLOW_SCORE          /* Allow the user to check his score (v-key)    */
-#define ALLOW_ARTIFACT_CHECK /* Allow the user to check artifacts            */
-#define ALLOW_CHECK_UNIQUES  /* Allow player to check (dead) unique monsters */
-#define TARGET               /* Enable targeting mode                        */
-#define AUTOROLLER           /* Allow autorolling of characters              */
-#undef NICE		             /* Be nice to other users during autorolling    */
 #define SATISFY_HUNGER		 /* Do 'satisfy hunger' rather then 'create food'
 								to reduce the tedium for handling food. -CWS */
+
+
+/*
+ * OPTION: for the AFS distributed file system, define this to ensure that
+ * the program is secure with respect to the setuid code, this prohibits
+ * inferior shells, also does not relinquish setuid priviledges at the start,
+ * but instead calls the AFS library routines bePlayer(), beGames(),
+ * and Authenticate().
+ */
+/* #define SECURE */
+
+
+/*
+ * OPTION: Allow the player to copy save files.  Turning this off
+ * may or may not stop players from doing it, though.  In fact, this
+ * option is not even referenced on SET_UID machines.
+ */
+#define ALLOW_FIDDLING
+
+
+/*
+ * OPTION: Allow checking of artifacts (in town)
+ */
+#define ALLOW_ARTIFACT_CHECK
+ 
+/*
+ * OPTION: Allow checking of dead uniques
+ */
+#define ALLOW_CHECK_UNIQUES
+
+/*
+ * OPTION: Allow characteres to be "auto-rolled"
+ */
+#define AUTOROLLER
+
+/*
+ * OPTION: Allow locations and monsters to be "targetted"
+ */
+#define TARGET
 
 
 /*****************************************************
@@ -48,6 +80,18 @@
 #define DEFAULT_PATH "./lib/"
 
 
+/*
+ * OPTION: On multiuser systems, be "nice" when autorolling.
+ */
+# define NICE
+
+
+/*
+ * OPTION: Check the "hours" file
+ */
+#undef CHECK_HOURS
+
+
 /* this sets the default user interface
  * to use the original key bindings (keypad for movement) set ROGUE_LIKE
  * to FALSE, to use the rogue-like key bindings (vi style movement)
@@ -56,14 +100,6 @@
 
 #define ROGUE_LIKE TRUE
 
-
-/* for the AFS distributed file system, define this to ensure that
-   the program is secure with respect to the setuid code, this prohibits
-   inferior shells, also does not relinquish setuid priviledges at the start,
-   but instead calls the AFS library routines bePlayer(), beGames(),
-   and Authenticate() */
-
-/* #define SECURE */
 
 
 
